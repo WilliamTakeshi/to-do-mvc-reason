b8ef177cc3a685bc18ffc99b1c7a7c8c87864077
William Takeshi Pereira <williamtpereira@gmail.com>
2020-01-09 17:58:51 -0300

    add check

--- src/App/TodoApp.bs.js
+++ src/App/TodoApp.bs.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var List = require("bs-platform/lib/js/list.js");
+var $$Array = require("bs-platform/lib/js/array.js");
 var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
 
@@ -8,29 +9,70 @@ function str(prim) {
   return prim;
 }
 
+var lastId = {
+  contents: 0
+};
+
 function newItem(param) {
+  lastId.contents = lastId.contents + 1 | 0;
   return {
-          title: "new thing to do",
-          completed: false
+          id: lastId.contents,
+          title: "Click a button",
+          completed: true
         };
 }
 
 function reducer(state, action) {
-  return {
-          items: /* :: */[
-            {
-              title: "new thing to do",
-              completed: false
-            },
-            state.items
-          ]
-        };
+  if (action) {
+    var id = action[0];
+    var items = List.map((function (item) {
+            var match = item.id === id;
+            if (match) {
+              return {
+                      id: item.id,
+                      title: item.title,
+                      completed: !item.completed
+                    };
+            } else {
+              return item;
+            }
+          }), state.items);
+    return {
+            items: items
+          };
+  } else {
+    return {
+            items: /* :: */[
+              newItem(/* () */0),
+              state.items
+            ]
+          };
+  }
 }
+
+function TodoApp$TodoItem(Props) {
+  var item = Props.item;
+  var onToggle = Props.onToggle;
+  return React.createElement("div", {
+              className: "item",
+              onClick: (function (_e) {
+                  return Curry._1(onToggle, /* () */0);
+                })
+            }, React.createElement("input", {
+                  checked: item.completed,
+                  type: "checkbox"
+                }), item.title);
+}
+
+var TodoItem = {
+  make: TodoApp$TodoItem
+};
 
 function TodoApp(Props) {
   var match = React.useReducer(reducer, {
         items: /* :: */[
           {
+            id: 0,
             title: "Write some things to do",
             completed: false
           },
@@ -38,7 +80,8 @@ function TodoApp(Props) {
         ]
       });
   var dispatch = match[1];
-  var numItems = List.length(match[0].items);
+  var items = match[0].items;
+  var numItems = List.length(items);
   return React.createElement("div", {
               className: "app"
             }, React.createElement("div", {
@@ -49,13 +92,23 @@ function TodoApp(Props) {
                         })
                     }, "Add something")), React.createElement("div", {
                   className: "items"
-                }, "Nothing"), React.createElement("div", undefined, String(numItems) + "items"));
+                }, $$Array.of_list(List.map((function (item) {
+                            return React.createElement(TodoApp$TodoItem, {
+                                        item: item,
+                                        onToggle: (function (param) {
+                                            return Curry._1(dispatch, /* ToggleItem */[item.id]);
+                                          }),
+                                        key: String(item.id)
+                                      });
+                          }), items))), React.createElement("div", undefined, String(numItems) + "items"));
 }
 
 var make = TodoApp;
 
 exports.str = str;
+exports.lastId = lastId;
 exports.newItem = newItem;
 exports.reducer = reducer;
+exports.TodoItem = TodoItem;
 exports.make = make;
 /* react Not a pure module */
--- src/App/TodoApp.re
+++ src/App/TodoApp.re
@@ -1,20 +1,43 @@
 type item = {
+  id: int,
   title: string,
   completed: bool,
 };
 
 type action =
-  | AddItem;
+  | AddItem
+  | ToggleItem(int);
 
 type state = {items: list(item)};
 
 let str = React.string;
 
-let newItem = () => {title: "new thing to do", completed: false};
+let lastId = ref(0);
+let newItem = () => {
+  lastId := lastId^ + 1;
+  {id: lastId^, title: "Click a button", completed: true};
+};
 
 let reducer = (state, action) => {
   switch (action) {
   | AddItem => {items: [newItem(), ...state.items]}
+  | ToggleItem(id) =>
+    let items =
+      List.map(
+        item => item.id === id ? {...item, completed: !item.completed} : item,
+        state.items,
+      );
+    {items: items};
+  };
+};
+
+module TodoItem = {
+  [@react.component]
+  let make = (~item, ~onToggle) => {
+    <div className="item" onClick={_e => onToggle()}>
+      <input type_="checkbox" checked={item.completed} />
+      {str(item.title)}
+    </div>;
   };
 };
 
@@ -23,7 +46,9 @@ let make = () => {
   let ({items}, dispatch) =
     React.useReducer(
       reducer,
-      {items: [{title: "Write some things to do", completed: false}]},
+      {
+        items: [{id: 0, title: "Write some things to do", completed: false}],
+      },
     );
   let numItems = List.length(items);
   <div className="app">
@@ -33,7 +58,19 @@ let make = () => {
         {str("Add something")}
       </button>
     </div>
-    <div className="items"> {str("Nothing")} </div>
+    <div className="items">
+      {List.map(
+         item =>
+           <TodoItem
+             item
+             key={string_of_int(item.id)}
+             onToggle={() => dispatch(ToggleItem(item.id))}
+           />,
+         items,
+       )
+       |> Array.of_list
+       |> React.array}
+    </div>
     <div> {str(string_of_int(numItems) ++ "items")} </div>
   </div>;
 };
